ca34b35baf892c17e7e464bed80aae4486a1a882
tak42 <1206ookubo@gmail.com>
2024-10-31 14:51:07 +0900

    style: change favoriteBtn

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -1,6 +1,9 @@
 "use strict";
 var alertBtn = document.createElement('button');
 alertBtn.innerHTML = 'Click me';
+alertBtn.style.height = '100px';
+alertBtn.style.width = '100px';
+alertBtn.style.backgroundColor = 'red';
 alertBtn.addEventListener('click', function () {
     alert('Hello');
 });
