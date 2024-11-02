f568e13d111a83440515b8c9218782b66c819d5a
tak42 <1206ookubo@gmail.com>
2024-11-02 09:51:57 +0900

    refactor: favoriteBtn.js

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -1,7 +1,21 @@
 "use strict";
-var alertBtn = document.createElement('input');
-var alertFunc = function () {
-    alert('Hello');
+var createBtn = function () {
+    return document.createElement('button');
 };
+var alertFunc = function () {
+    alert('Hello World!!');
+};
+var btnStyle = {
+    height: '50px',
+    width: '100px',
+    backgroundColor: 'gray',
+};
+var setBtnStyle = function (bthElm, style) {
+    Object.keys(style).forEach(function (key) {
+        bthElm.style.setProperty(key, style[key]);
+    });
+};
+var alertBtn = createBtn();
+setBtnStyle(alertBtn, btnStyle);
 alertBtn.addEventListener('click', alertFunc);
 document.body.appendChild(alertBtn);
