ee950fe0f7029fe880c97e494b1ef784c3f24996
tak42 <1206ookubo@gmail.com>
2024-11-05 00:03:13 +0900

    refactor: favoriteBtn.js

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -1,35 +1,21 @@
-"use strict";
-// type StyleProperty = 'height' | 'width' | 'color' | 'background-color';
-// type Style = { [key in StyleProperty]: string };
+'use strict';
 var createBtn = function () {
-    return document.createElement('button');
+  return document.createElement('button');
 };
 var alertFunc = function () {
-    alert('Hello World!!');
+  alert('Hello World!!');
 };
-// const btnStyle: Style = {
-//   height: '50px',
-//   width: '100px',
-//   color: 'white',
-//   'background-color': 'red',
-// };
-// const setBtnStyle = (btnElm: HTMLButtonElement, style: Style) => {
-//   Object.entries(style).forEach(([key, val]) => {
-//     btnElm.style.setProperty(key, val);
-//   });
-// };
-function setBtnStyle(property, value, btnElm) {
-    btnElm.style[property] = value;
-}
+var setBtnStyle = function (property, value, btnElm) {
+  btnElm.style[property] = value;
+};
 var setBtnInnerText = function (btnElm, txt) {
-    btnElm.innerText = txt;
+  btnElm.innerText = txt;
 };
 var alertBtn = createBtn();
-setBtnInnerText(alertBtn, 'アラート表示3');
+setBtnInnerText(alertBtn, 'アラート表示');
 setBtnStyle('height', '50px', alertBtn);
 setBtnStyle('width', '100px', alertBtn);
 setBtnStyle('color', 'white', alertBtn);
 setBtnStyle('backgroundColor', 'red', alertBtn);
-// setBtnStyle(alertBtn, btnStyle);
 alertBtn.addEventListener('click', alertFunc);
 document.body.appendChild(alertBtn);
