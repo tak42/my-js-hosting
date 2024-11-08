051797bc5fd9edd0974cf8c7deaad6b64c7e465b
tak42 <1206ookubo@gmail.com>
2024-11-09 01:41:36 +0900

    refactor: favoriteBtn.js

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -5,16 +5,12 @@ var btnStyle = [
     { property: 'color', value: 'red' },
 ];
 var iframeStyle = [
+    { property: 'height', value: '800px' },
+    { property: 'width', value: '600px' },
     { property: 'position', value: 'absolute' },
     { property: 'top', value: '50%' },
     { property: 'left', value: '50%' },
     { property: 'transform', value: 'translate(-50%, -50%)' },
-];
-var divStyle = [
-    { property: 'height', value: '100vh' },
-    { property: 'display', value: 'flex' },
-    { property: 'justifyContent', value: 'center' },
-    { property: 'alignItems', value: 'center' },
 ];
 var setStyle = function (htmlElm, styles) {
     styles.forEach(function (val) {
@@ -25,13 +21,10 @@ var setDisplayIframe = function (child) {
     document.body.appendChild(child);
 };
 var btn = document.createElement('button');
-var div = document.createElement('div');
 var iframe = document.createElement('iframe');
 btn.innerText = 'iframe 表示';
 iframe.src = 'https://www.mouse-jp.co.jp/';
 setStyle(btn, btnStyle);
-setStyle(div, divStyle);
 setStyle(iframe, iframeStyle);
 btn.addEventListener('click', function () { return setDisplayIframe(iframe); });
-div.appendChild(iframe);
 document.body.appendChild(btn);
