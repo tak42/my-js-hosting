4056f0d0149f8b7d951f237395d86e1910b87f57
tak42 <1206ookubo@gmail.com>
2024-11-09 00:52:39 +0900

    refactor: favoriteBtn.js

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -1,31 +1,19 @@
 "use strict";
-var createBtn = function () {
-    return document.createElement('button');
+var btnStyle = [
+    { property: 'width', value: '100px' },
+    { property: 'height', value: '50px' },
+    { property: 'backgroundColor', value: 'red' },
+];
+var alertFunc = function () {
+    alert('Hello World!!');
 };
-var createIframe = function () {
-    return document.createElement('iframe');
+var setStyle = function (htmlElm, styles) {
+    styles.forEach(function (val) {
+        htmlElm.style.setProperty(val.property, val.value);
+    });
 };
-var setStyle = function (property, value, elm) {
-    elm.style[property] = value;
-};
-var setBtnInnerText = function (btnElm, txt) {
-    btnElm.innerText = txt;
-};
-var setIframeSoc = function (ifElm, url) {
-    ifElm.src = url;
-};
-var displayIframe = function () {
-    var iframe = createIframe();
-    setStyle('height', '200px', iframe);
-    setStyle('width', '300px', iframe);
-    setIframeSoc(iframe, 'https://web.vsmobile.jp/exvs2ob/login');
-    document.body.appendChild(iframe);
-};
-var alertBtn = createBtn();
-setBtnInnerText(alertBtn, 'iframe表示');
-setStyle('height', '50px', alertBtn);
-setStyle('width', '100px', alertBtn);
-setStyle('color', 'white', alertBtn);
-setStyle('backgroundColor', 'red', alertBtn);
-alertBtn.addEventListener('click', displayIframe);
-document.body.appendChild(alertBtn);
+var btn = document.createElement('button');
+btn.innerText = 'アラート表示';
+setStyle(btn, btnStyle);
+btn.addEventListener('click', alertFunc);
+document.body.appendChild(btn);
