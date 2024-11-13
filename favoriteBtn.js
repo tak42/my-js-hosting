b71b31199b2a927cfab7f3729f18fd5a191a0192
tak42 <1206ookubo@gmail.com>
2024-11-13 23:10:31 +0900

    refactor: favoriteBtn.ts

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -3,6 +3,9 @@ var btnStyle = [
     { property: 'width', value: '100px' },
     { property: 'height', value: '50px' },
     { property: 'color', value: 'red' },
+    { property: 'position', value: 'absolute' },
+    { property: 'top', value: '100px' },
+    { property: 'left', value: '100px' },
 ];
 var iframeStyle = [
     { property: 'position', value: 'absolute' },
@@ -21,7 +24,7 @@ var setDisplayIframe = function (iframe) {
 var btn = document.createElement('button');
 var iframe = document.createElement('iframe');
 btn.innerText = 'iframe 表示';
-iframe.src = 'http://localhost:3000/';
+iframe.src = 'https://localhost:3000/';
 setStyle(btn, btnStyle);
 setStyle(iframe, iframeStyle);
 btn.addEventListener('click', function () { return setDisplayIframe(iframe); });
