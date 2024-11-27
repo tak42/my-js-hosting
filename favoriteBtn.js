0d16d47764bfc3e210b0df92aa0493923450f597
tak42 <1206ookubo@gmail.com>
2024-11-27 12:15:20 +0900

    refactor: favoriteBtn.ts

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -1,13 +1,11 @@
 "use strict";
-var localhostUrl = 'http://localhost:3000';
-// const uuid = crypto.randomUUID();
-// const containerId = `feasy_iframeContainer_${uuid}`;
+var LOCALHOST_URL = 'http://localhost:3000';
 var btnStyle = [
-    { property: 'width', value: '100px' },
+    { property: 'width', value: '150px' },
     { property: 'height', value: '50px' },
     { property: 'color', value: 'red' },
     { property: 'position', value: 'absolute' },
-    { property: 'top', value: '100px' },
+    { property: 'top', value: '20%' },
     { property: 'left', value: '100px' },
 ];
 var containerStyle = [
@@ -17,6 +15,7 @@ var containerStyle = [
     { property: 'top', value: '50%' },
     { property: 'left', value: '50%' },
     { property: 'transform', value: 'translate(-50%, -50%)' },
+    { property: 'backgroundColor', value: 'white' },
 ];
 var iframeStyle = [
     { property: 'height', value: '100%' },
@@ -28,27 +27,36 @@ var setStyle = function (htmlElm, styles) {
     });
 };
 var showIframe = function () {
-    // const container = document.getElementById(containerId);
     var iframe = document.createElement('iframe');
-    iframe.src = localhostUrl;
+    iframe.src = LOCALHOST_URL;
     iframe.sandbox.value = 'allow-scripts allow-same-origin';
     setStyle(iframe, iframeStyle);
     container.appendChild(iframe);
 };
 var hideIframe = function () {
-    // const container = document.getElementById(containerId);
     var iframe = container.querySelector('iframe');
     if (iframe)
         container.removeChild(iframe);
 };
+var shareForm = function (data) {
+    var nameInput = document.getElementById('q1');
+    if (!nameInput)
+        console.log("don't get q1");
+    console.log(data);
+};
+var iframePostActions = [
+    { action: 'hide', func: hideIframe },
+    { action: 'share', func: shareForm },
+];
 window.addEventListener('message', function (event) {
-    if (event.origin === localhostUrl)
-        hideIframe();
+    var actionFunc = iframePostActions.filter(function (val) { return val.action === event.data.action; });
+    if (event.origin !== LOCALHOST_URL)
+        return console.log('not localhost');
+    actionFunc[0].func(event.data.content);
 });
 var btn = document.createElement('button');
 var container = document.createElement('div');
 btn.innerText = 'iframe 表示';
-// container.id = containerId;
 setStyle(btn, btnStyle);
 setStyle(container, containerStyle);
 btn.addEventListener('click', showIframe);
