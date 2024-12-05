837293c3ae7079b4ec306866afdf8121c244cd87
tak42 <1206ookubo@gmail.com>
2024-12-05 23:44:39 +0900

    refactor: favoriteBtn.ts

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -1,12 +1,12 @@
 "use strict";
 var LOCALHOST_URL = 'http://localhost:3000';
 var btnStyle = [
-    { property: 'width', value: '150px' },
-    { property: 'height', value: '50px' },
+    { property: 'width', value: '160px' },
+    { property: 'height', value: '56px' },
     { property: 'color', value: 'red' },
     { property: 'position', value: 'absolute' },
-    { property: 'top', value: '20%' },
-    { property: 'left', value: '100px' },
+    { property: 'top', value: '16%' },
+    { property: 'left', value: '80px' },
 ];
 var containerStyle = [
     { property: 'height', value: '50%' },
@@ -34,6 +34,7 @@ var showIframe = function () {
     var iframe = document.createElement('iframe');
     iframe.src = LOCALHOST_URL;
     iframe.sandbox.value = 'allow-scripts allow-same-origin';
+    setStyle(container, containerStyle);
     setStyle(iframe, iframeStyle);
     container.appendChild(iframe);
 };
@@ -54,43 +55,33 @@ var combineIdentifiers = [
         kind: 'old',
     },
 ];
-var searchTargetInputs = function () {
-    return Array.from(document.getElementsByTagName('input')).filter(function (elm) {
-        return combineIdentifiers.some(function (val) { return val.id === elm.id; });
-    });
-};
-var shareForm = function (contents) {
-    var inputElms = searchTargetInputs();
-    if (!inputElms.length)
-        return false;
+var shareForm = function (content) {
+    var inputElms = Array.from(document.getElementsByTagName('input'));
     combineIdentifiers.forEach(function (val) {
-        var content = contents.find(function (_a) {
-            var kind = _a.kind;
-            return kind === val.kind;
-        });
-        inputElms.filter(function (_a) {
+        var elm = inputElms.find(function (_a) {
             var id = _a.id;
             return id === val.id;
-        })[0].value = content ? content.value : '';
+        });
+        if (!elm)
+            return;
+        elm.value = content[val.kind];
     });
     hideIframe();
 };
-var iframePostActions = [
-    { action: 'hide', func: hideIframe },
-    { action: 'share', func: shareForm },
-];
+var iframePostActions = {
+    hide: hideIframe,
+    share: shareForm,
+};
 window.addEventListener('message', function (event) {
     if (event.origin !== LOCALHOST_URL)
-        return false;
+        return;
     var postData = event.data;
-    var actionFunc = iframePostActions.filter(function (val) { return val.action === postData.action; });
-    actionFunc[0].func(postData.contents);
+    iframePostActions[postData.action](postData.content);
 });
 var btn = document.createElement('button');
 var container = document.createElement('div');
 btn.innerText = 'iframe 表示';
 setStyle(btn, btnStyle);
-setStyle(container, containerStyle);
 btn.addEventListener('click', showIframe);
 document.body.appendChild(btn);
 document.body.appendChild(container);
