6606266ce114e851d3f9e7efc5300badc2adf561
tak42 <nengdengguihui@gmail.com>
2024-12-24 23:55:56 +0900

    refactor:favoriteBtn.js

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -1,79 +1,32 @@
-var LOCALHOST_URL = 'http://localhost:3000';
-var btnStyle = [
-    { property: 'width', value: '160px' },
-    { property: 'height', value: '56px' },
-    { property: 'color', value: 'red' },
-    { property: 'position', value: 'absolute' },
-    { property: 'top', value: '160px' },
-    { property: 'left', value: '80px' },
-];
-var containerStyle = [
-    { property: 'height', value: '50%' },
-    { property: 'width', value: '50%' },
-    { property: 'position', value: 'absolute' },
-    { property: 'top', value: '50%' },
-    { property: 'left', value: '50%' },
-    { property: 'transform', value: 'translate(-50%, -50%)' },
-];
-var containerHideStyle = [
-    { property: 'height', value: '0' },
-    { property: 'width', value: '0' },
-];
-var iframeStyle = [
-    { property: 'height', value: '100%' },
-    { property: 'width', value: '100%' },
-    { property: 'background', value: 'white' },
-];
-var setStyle = function (htmlElm, styles) {
-    styles.forEach(function (val) {
-        htmlElm.style.setProperty(String(val.property), val.value);
-    });
-};
-var showIframe = function () {
-    setStyle(container, containerStyle);
-    if (container.querySelector('iframe'))
-        return;
-    var iframe = document.createElement('iframe');
-    iframe.src = LOCALHOST_URL;
-    iframe.sandbox.value = 'allow-scripts allow-same-origin';
-    setStyle(iframe, iframeStyle);
-    container.appendChild(iframe);
-};
-var hideIframe = function () {
-    setStyle(container, containerHideStyle);
-};
-var combineIdentifiers = [
-    {
-        id: 'q1',
-        kind: 'name',
-    },
-    {
-        id: 'q2',
-        kind: 'email',
-    },
-    {
-        id: 'q5',
-        kind: 'old',
-    },
-];
+import { LOCALHOST_URL } from '../common';
+// import { combineIdentifiers } from '../hooks/combineIdentifiers';
+import { hideIframe, showIframe } from '../hooks/iframe';
+import { btnStyle, setStyle } from '../hooks/styles';
 var shareForm = function (content) {
-    var inputElms = Array.from(document.getElementsByTagName('input'));
-    combineIdentifiers.forEach(function (val) {
-        var elm = inputElms.find(function (_a) {
-            var id = _a.id;
-            return id === val.id;
-        });
-        if (!elm)
-            return;
-        elm.value = content[val.kind];
-    });
-    hideIframe();
+    console.log(content);
+    // const inputElms = Array.from(document.getElementsByTagName('input'));
+    // combineIdentifiers.forEach((val) => {
+    //   const elm = inputElms.find(({ id }) => id === val.id);
+    //   if (!elm) return;
+    //   elm.value = content[val.kind];
+    // });
+    hideIframe(container);
 };
+var hideForm = function () {
+    hideIframe(container);
+};
+// const getOrigin = (event: MessageEvent<any>) => {
+//   event.source.postMessage(window.location.origin, event.origin);
+// }
 var iframePostActions = {
-    hide: hideIframe,
+    hide: hideForm,
     share: shareForm,
 };
 window.addEventListener('message', function (event) {
+    if (event.source === null)
+        return;
+    var parentOrigin = event.source.postMessage(window.location.origin, event.origin);
+    console.log(parentOrigin);
     if (event.origin !== LOCALHOST_URL)
         return;
     var postData = event.data;
@@ -83,7 +36,6 @@ var btn = document.createElement('button');
 var container = document.createElement('div');
 btn.innerText = 'iframe 表示';
 setStyle(btn, btnStyle);
-btn.addEventListener('click', showIframe);
+btn.addEventListener('click', function () { return showIframe(container); });
 document.body.appendChild(btn);
 document.body.appendChild(container);
-export {};
