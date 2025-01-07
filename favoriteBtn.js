3cfaf544e7516da3321d9a529fe112ac066e61b3
tak42 <nengdengguihui@gmail.com>
2025-01-07 15:03:53 +0900

    refactor: favoriteBtn.js

--- favoriteBtn.js
+++ favoriteBtn.js
@@ -1,7 +1,47 @@
-import { LOCALHOST_URL } from '../common';
-// import { combineIdentifiers } from '../hooks/combineIdentifiers';
-import { hideIframe, showIframe } from '../hooks/iframe';
-import { btnStyle, setStyle } from '../hooks/styles';
+var LOCALHOST_URL = 'http://localhost:3000';
+var btnStyle = [
+    { property: 'width', value: '160px' },
+    { property: 'height', value: '56px' },
+    { property: 'color', value: 'red' },
+    { property: 'position', value: 'absolute' },
+    { property: 'top', value: '160px' },
+    { property: 'left', value: '80px' },
+];
+var containerStyle = [
+    { property: 'height', value: '50%' },
+    { property: 'width', value: '50%' },
+    { property: 'position', value: 'absolute' },
+    { property: 'top', value: '50%' },
+    { property: 'left', value: '50%' },
+    { property: 'transform', value: 'translate(-50%, -50%)' },
+];
+var containerHideStyle = [
+    { property: 'height', value: '0' },
+    { property: 'width', value: '0' },
+];
+var iframeStyle = [
+    { property: 'height', value: '100%' },
+    { property: 'width', value: '100%' },
+    { property: 'background', value: 'white' },
+];
+var setStyle = function (htmlElm, styles) {
+    styles.forEach(function (val) {
+        htmlElm.style.setProperty(String(val.property), val.value);
+    });
+};
+var showIframe = function (container) {
+    setStyle(container, containerStyle);
+    if (container.querySelector('iframe'))
+        return;
+    var iframe = document.createElement('iframe');
+    iframe.src = LOCALHOST_URL;
+    iframe.sandbox.value = 'allow-scripts allow-same-origin';
+    setStyle(iframe, iframeStyle);
+    container.appendChild(iframe);
+};
+var hideIframe = function (container) {
+    setStyle(container, containerHideStyle);
+};
 var shareForm = function (content) {
     console.log(content);
     // const inputElms = Array.from(document.getElementsByTagName('input'));
@@ -39,3 +79,4 @@ setStyle(btn, btnStyle);
 btn.addEventListener('click', function () { return showIframe(container); });
 document.body.appendChild(btn);
 document.body.appendChild(container);
+export {};
